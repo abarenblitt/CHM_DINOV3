f81443d7964c6a3ba94c77fc7a6b47132bbece8c
[abarenblitt] <acbarenblitt@gmail.com>
2026-09-25 08:12:16 -0700

    fixing for model trianing

--- run_train.sh
+++ run_train.sh
@@ -3,8 +3,6 @@ set -e
 
 # 1. Catch positional arguments automatically sent by MAAP
 HF_TOKEN=$1
-TIF_URL=$2
-THRESHOLD=$3
 
 # 2. Set directories
 basedir=$( cd "$(dirname "$0")" ; pwd -P )
@@ -18,9 +16,7 @@ pip install torch torchvision transformers datasets rasterio evaluate scikit-lea
 
 # 4. RUN SCRIPT
 echo "Running Python script..."
-python DINOV2Segmentation.py \
+python training.py \
     --hf_token "${HF_TOKEN}" \
-    --tif_url "${TIF_URL}" \
-    --model_path "./model/model.pt" \
     --output_dir "${OUTPUT_DIR}" \
     --threshold "${THRESHOLD}"
